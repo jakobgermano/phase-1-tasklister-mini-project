413ae915c33d000db68dbe6bf439f9ce34b83253
jakob <jakob.germano@gmail.com>
2021-07-01 18:18:54 -0600

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const newTaskForm = document.querySelector('form');
+  const inputBox = document.getElementsByTagName('new-task-description');
+  const listBox = document.querySelector('ul#tasks');
+
+newTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let newTask = document.createElement('li')
+  listBox.appendChild(newTask)
+  newTask.innerText = inputBox
+})
+
+
